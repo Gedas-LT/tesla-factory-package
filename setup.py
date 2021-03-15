f650e5de66b34dc9dad2efa685e068d57aa052f0
Gediminas Šimavičius <gediminas.simavicius@gmail.com>
2021-03-15 22:40:42 +0200

    Changed the setup.py file

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
 import setuptools
 
-setuptools.setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tesla-factory-pkg-gedas-lt",
+    version="0.0.1",
+    author="Gediminas Šimavičius",
+    author_email="gediminas.simavicius@gmail.com",
+    description="Package for creating Tesla cars",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gedas-LT/tc-tesla-factory-test-pkg",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "tesla"},
+    packages=setuptools.find_packages(where="tesla"),
+    python_requires=">=3.6",
+)
