e0397c95446ca3f2d3662bd5d3188d679363d6f9
Gediminas Šimavičius <gediminas.simavicius@gmail.com>
2021-03-15 22:46:55 +0200

    Changed packaged name and URL

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tesla-factory-pkg-gedas-lt",
+    name="tesla-factory-package",
     version="0.0.1",
     author="Gediminas Šimavičius",
     author_email="gediminas.simavicius@gmail.com",
     description="Package for creating Tesla cars",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Gedas-LT/tc-tesla-factory-test-pkg",
+    url="https://github.com/Gedas-LT/tesla-factory-package",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
